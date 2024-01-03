1f25767a2743796ae32bff7a8abd14c7f3c7dbaa
AhmedSalahZero <55031551+AhmedSalahZero@users.noreply.github.com>
2024-01-03 19:23:16 +0200

    add yml file

--- database/migrations/2023_09_21_165047_create_travel_trakings_table.php
+++ database/migrations/2023_09_21_165047_create_travel_trakings_table.php
@@ -6,11 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateTravelTrakingsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('travel_trakings', function (Blueprint $table) {
