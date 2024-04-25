3070354a500490a4cc19f5d4390a15f39e5cd86c
AhmedSalahZero <55031551+AhmedSalahZero@users.noreply.github.com>
2024-04-25 02:10:29 +0200

    add slider seeder

---
+++ database/factories/SliderFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SliderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+           
+            // 'image'=>$this->faker->image('public/storage/CarMake',400,300, null, false),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+		$this->call(SliderSeeder::class);
 		$this->call(InformationSeeder::class);
 		$this->call(HelpSeeder::class);
 		$this->call(PromotionSeeder::class);
