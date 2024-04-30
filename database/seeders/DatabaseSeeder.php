c8220f36374c2c95a053c669fb1b173879ec4746
AhmedSalahZero <55031551+AhmedSalahZero@users.noreply.github.com>
2024-05-01 02:12:47 +0300

    no limit memory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,25 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+		$this->call(CountrySeeder::class);
+		$this->call(CarSizeSeeder::class);
+		$this->call(CarMakeSeeder::class);
+		$this->call(CarModelSeeder::class);
+		$this->call(CitySeeder::class);
+		$this->call(DriverSeeder::class);
 		$this->call(SliderSeeder::class);
 		$this->call(InformationSeeder::class);
 		$this->call(HelpSeeder::class);
 		$this->call(PromotionSeeder::class);
 		$this->call(TravelConditionSeeder::class);
+		$this->call(RoleSeeder::class);
+		$this->call(AdminSeeder::class);
+		$this->call(ClientSeeder::class);
 		$this->call(CancellationReasonSeeder::class);
-		// $this->call(RoleSeeder::class);
-		// $this->call(AdminSeeder::class);
-		// $this->call(CountrySeeder::class);
-		// $this->call(CarSizeSeeder::class);
-		// $this->call(CarMakeSeeder::class);
-		// $this->call(CarModelSeeder::class);
-		// $this->call(CitySeeder::class);
-		// $this->call(DriverSeeder::class);
-		// $this->call(ClientSeeder::class);
-		// $this->call(EmergencyContactsSeeder::class);
-		// $this->call(CouponSeeder::class);
-		// $this->call(TravelSeeder::class);
-		// $this->call(SupportTicketSeeder::class);
+		$this->call(EmergencyContactsSeeder::class);
+		$this->call(CouponSeeder::class);
+		$this->call(TravelSeeder::class);
+		$this->call(SupportTicketSeeder::class);
 		
 		Country::whereIn('id',[194,65])->update([
 			'cancellation_fees_for_client'=>10,
