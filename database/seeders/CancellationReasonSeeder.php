de1e47462d6140fd469765daf67424ae7c4327d4
AhmedSalahZero <55031551+AhmedSalahZero@users.noreply.github.com>
2024-05-01 02:43:31 +0300

    max execution

--- database/seeders/CancellationReasonSeeder.php
+++ database/seeders/CancellationReasonSeeder.php
@@ -36,6 +36,9 @@ class CancellationReasonSeeder extends Seeder
      */
     public function run()
     {
+		ini_set('memory_limit', -1);
+		ini_set('max_execution_time', 0);
+
 		CancellationReason::factory()->create([
 			'name_en'=>'Client did not come',
 			'name_ar'=>'العميل لم ياتي',
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
+		
+		ini_set('memory_limit', -1);
+		ini_set('max_execution_time', 0);
+
 		$this->call(CountrySeeder::class);
 		$this->call(CarSizeSeeder::class);
 		$this->call(CarMakeSeeder::class);
