31fb64360841751e1d5d533c7d58bc6bd98bc1cd
AhmedSalahZero <55031551+AhmedSalahZero@users.noreply.github.com>
2024-05-01 02:45:38 +0300

    max exeution time

--- database/seeders/CancellationReasonSeeder.php
+++ database/seeders/CancellationReasonSeeder.php
@@ -36,9 +36,6 @@ class CancellationReasonSeeder extends Seeder
      */
     public function run()
     {
-		ini_set('memory_limit', -1);
-		ini_set('max_execution_time', 0);
-
 		CancellationReason::factory()->create([
 			'name_en'=>'Client did not come',
 			'name_ar'=>'العميل لم ياتي',
